fc1eb91988538821a9e0648367c7356c5fd6d041
juntaozhou05 <350529307@qq.com>
2018-03-29 11:49:54 +0800

    add lsit

--- index1.js
+++ index1.js
@@ -9,10 +9,10 @@ function isIn() {
       if (
         nvert[i].longitude > testy != nvert[j].longitude > testy &&
         testx <
-          (nvert[j].latitude - nvert[i].latitude) *
-            (testy - nvert[i].longitude) /
-            (nvert[j].longitude - nvert[i].longitude) +
-            nvert[i].latitude
+        (nvert[j].latitude - nvert[i].latitude) *
+        (testy - nvert[i].longitude) /
+        (nvert[j].longitude - nvert[i].longitude) +
+        nvert[i].latitude
       )
         c = !c;
     }
@@ -21,44 +21,118 @@ function isIn() {
 }
 
 //单链表操作
-
-//声明一个链表类
 function Node(element) {
   this.element = element;
   this.next = null;
 }
-
-//链表的操作
 function LList() {
-  this.head = new node("head");
+  this.head = new Node("head");
   this.find = find;
   this.insert = insert;
   this.remove = remove;
   this.display = display;
 }
-
-//查找元素
 function find(val) {
-  let currentNode = this.head;
-  while (currentNode.element != val) {
-    currentNode = currentNode.next;
+  let current = this.head;
+  while (current.element !== val) {
+    current = current.next;
   }
-  return currentNode;
+  return current.element;
 }
 
-//插入元素
 function insert(target, val) {
   let newNode = new Node(target);
-  let currentNode = this.find(newNode);
-  newNode.next = currentNode.next;
-  currentNode.next = newNode;
+  let current = find(val);
+  newNode.next = current.next;
+  current.next = newNode;
 }
 
-//打印链表
+function findPrevious(val) {
+  let current = this.head;
+  while (current.next !== null) {
+    if (current.next.element !== val) {
+      current = current.next;
+    }
+    return current;
+  }
+}
+
+function remove(val) {
+  let preNode = findPrevious(val);
+  if (preNode.next !== null) {
+    preNode.next = preNode.next.next;
+  }
+}
+
 function display() {
-  let currentNode = this.head;
-  while (currentNode.next !== null) {
-    console.log(currentNode.next.element);
-    currentNode = currentNode.next;
+  let current = this.head;
+  while (current.next !== null) {
+    console.log(current.element);
+    current = current.next;
+  }
+}
+
+//查找单链表二分之一的元素
+function findHalf() {
+  let mid = this.head;
+  let search = this.head;
+  while (search.next !== null) {
+    if (search.next.next !== null) {
+      search = search.next.next;
+      mid = mid.next;
+    } else {
+      search = search.next;
+    }
+  }
+  return mid.element;
+}
+
+//循环链表
+function LList() {
+  this.head = new Node("head")
+  this.head.next = this.head
+  this.find = dFind
+  this.insert = dInsert
+  this.remove = dRemove
+  this.display = dDistplay
+}
+
+function dFind(val) {
+  let current = this.head
+  while (current.next !== null && current.next.element !== "head") {
+    current = current.next
+  }
+  return current.element
+}
+
+function dInsert(target, val) {
+  let newNode = new NOde(val)
+  let current = dFind(target)
+  newNode.next = current.next
+  current.next = newNode
+}
+
+function findPre(val) {
+  let current = this.head
+  while (current.next !== null && current.next.element !== "head") {
+    if (current.next.element !== val) {
+      current = current.next
+    }
+    return current
+  }
+}
+
+function dRemove(val) {
+  let current = findPre(val)
+  if (current.next !== null && current.next.element !== "head") {
+    current.next = current.next.next
+  }
+}
+
+function dDisplay() {
+  let current = this.head
+  while (current.next !== null && current.next.element !== "head") {
+    console.log(current.element)
+    current = current.next
   }
 }
